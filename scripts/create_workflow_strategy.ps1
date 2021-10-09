b8b898ac3a752ee1801b050bcf115e1e2a44a58d
Eric van Wijk <eric@van-wijk.com>
2021-10-09 19:44:37 +0200

    Use create_workflow_strategy.ps1

--- scripts/create_workflow_strategy.ps1
+++ scripts/create_workflow_strategy.ps1
@@ -1,4 +1,24 @@
 #!/usr/bin/env pwsh
 
+$preferredTerraformVersion = (Get-Content $PSScriptRoot/../terraform/.terraform-version | Out-String) -replace "`n|`r"
+Write-Output "::set-output name=terraform_preferred_version::${preferredTerraformVersion}"
+$latestTerraformVersion = (Invoke-WebRequest -Uri https://checkpoint-api.hashicorp.com/v1/check/terraform -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty "current_version")
+Write-Output "::set-output name=terraform_latest_version::${latestTerraformVersion}"
 
-@{"Metadata"="true"}
+$installedAzureCLIVersion = $(az version --query '\"azure-cli\"' -o tsv)
+Write-Output "::set-output name=azure_cli_installed_version::${installedAzureCLIVersion}"
+$latestAzureCLIVersion = (Invoke-WebRequest -Uri https://api.github.com/repos/Azure/azure-cli/releases/latest -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty "name").split(" ")[-1]
+Write-Output "::set-output name=azure_cli_latest_version::${latestAzureCLIVersion}"
+
+$matrixObject = (Get-Content $PSScriptRoot/../.github/workflows/ci-scripted-strategy.json | ConvertFrom-Json) 
+
+# Current / preferred versions
+$matrixObject.include[0].terraform_version = $preferredTerraformVersion
+$matrixObject.include[0].azure_cli_version = $installedAzureCLIVersion
+
+# Latest versions
+$matrixObject.include[1].terraform_version = $latestTerraformVersion
+$matrixObject.include[1].azure_cli_version = $latestAzureCLIVersion
+
+$matrix = ($matrixObject | ConvertTo-Json)
+Write-Output "::set-output name=matrix::${matrix}"
