bf4c28250f898dc4eebd609731e68c618f8e4b18
Eric van Wijk <eric@van-wijk.com>
2022-01-09 15:31:59 +0100

    Increase Log Analytics retention

--- terraform/management.tf
+++ terraform/management.tf
@@ -3,7 +3,7 @@ resource azurerm_log_analytics_workspace monitor {
   resource_group_name          = azurerm_resource_group.minecraft.name
   location                     = azurerm_resource_group.minecraft.location
   sku                          = "PerGB2018"
-  retention_in_days            = 30
+  retention_in_days            = 365
 
   tags                         = local.tags
 }
