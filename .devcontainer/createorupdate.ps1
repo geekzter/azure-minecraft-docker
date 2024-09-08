4d6e2fc5cd3e34e71f3214dd14c939f41a665d83
Matt Bishop <matt@withinfocus.com>
2024-09-08 17:06:42 -0400

    Use feature for Azure Functions tools too

--- .devcontainer/createorupdate.ps1
+++ .devcontainer/createorupdate.ps1
@@ -1,14 +1,8 @@
 #!/usr/bin/env pwsh
 # Runs post create commands to prep Codespace for project
 
-# Update relevant packages
+# Get latest package information
 sudo apt-get update
-
-# Install Azure Functions Core Tools
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/debian/$(lsb_release -rs 2>/dev/null | cut -d'.' -f 1)/prod $(lsb_release -cs 2>/dev/null) main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-get install -y azure-functions-core-tools-4
 
 # Install tfenv dependencies
 sudo apt-get install -y tmux
